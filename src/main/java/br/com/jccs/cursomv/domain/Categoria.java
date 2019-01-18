16f0f08b4ba2310a09e6e0fcfa46aa6cbcf11b24
carvalhojccs <jonne.cley@gmail.com>
2019-01-18 19:36:39 -0300

    Configuração do Mysql e criação de base de dados

--- src/main/java/br/com/jccs/cursomv/domain/Categoria.java
+++ src/main/java/br/com/jccs/cursomv/domain/Categoria.java
@@ -2,10 +2,17 @@ package br.com.jccs.cursomv.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String nome;
     
