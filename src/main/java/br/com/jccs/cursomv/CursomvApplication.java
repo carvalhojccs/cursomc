33fc1312b2f275cfa800cc9a1d906a27bb2da806
carvalhojccs <jonne.cley@gmail.com>
2019-01-18 20:41:00 -0300

    Criando operção de instanciação

--- src/main/java/br/com/jccs/cursomv/CursomvApplication.java
+++ src/main/java/br/com/jccs/cursomv/CursomvApplication.java
@@ -1,14 +1,31 @@
 package br.com.jccs.cursomv;
 
+import br.com.jccs.cursomv.domain.Categoria;
+import br.com.jccs.cursomv.repositories.CategoriaRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomvApplication {
+public class CursomvApplication implements CommandLineRunner{
 
+        @Autowired
+        private CategoriaRepository categoriaRepository;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(CursomvApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 
 }
 
